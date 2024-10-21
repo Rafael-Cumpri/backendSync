504078aac61dee31e7603b286474df087de424ec
Maria <maria.cancian@aluno.senai.br>
2024-10-21 10:31:11 -0300

    fix

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -6,11 +6,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-<<<<<<< HEAD
   client_encoding: 'UTF8' // Configura o encoding
-=======
-
->>>>>>> a242227578269342ab000b7319a8fc6e9a5d40ef
 });
 
 module.exports = pool;
