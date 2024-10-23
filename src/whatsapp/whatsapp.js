d4c0a9a97e05310101a9219ae8a66b96aa9e4dca
Rafael-Cumpri <rafael.cumpri@aluno.senai.br>
2024-10-23 16:20:39 -0300

    feat: add mensagem agendada

--- src/whatsapp/whatsapp.js
+++ src/whatsapp/whatsapp.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const { CronJob } =  require('cron');
+
 
 const app = express();
 const PORT = 3000; // Porta do seu backend
@@ -25,6 +27,29 @@ app.post('/send-message', async (req, res) => {
   }
 });
 
+// agendar a msg 
+const job = new CronJob(
+  '04 15 * * 1-5',
+  async function() {
+    const phone= '5519991263454';
+    const message = 'DEVOLVE A CHAVE AGORA';
+
+    try{
+      await axios.post('http://localhost:3000/api/sendText',{
+        chatId: `${phone}@c.us`,
+        text: message,
+        session: 'default',
+      })
+      console.log(`msg para ${phone} : ${message}`);
+    }catch (error){
+      console.error('erro ao enviar msg', error)
+    }
+    
+  },
+  null,
+   true,
+  'America/Sao_Paulo'
+)
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:3000`);
 });
