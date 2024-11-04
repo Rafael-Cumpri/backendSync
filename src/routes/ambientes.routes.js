addc43785c6bb9e7c110924b2cbbeb4dc89660cd
giobasiliox <giovana.basilio@aluno.senai.br>
2024-11-04 16:28:54 -0300

    feat: atualizar rotas de ambientes para incluir busca por nome e renomear função de edição

--- src/routes/ambientes.routes.js
+++ src/routes/ambientes.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { postAmbientes, getAmbientes, upload, deleteAmbientes, editAmbientes } = require('../controllers/ambientes.controllers');
+const { postAmbientes, getAmbientes, upload, deleteAmbientes, updateAmbiente, getAmbienteByName } = require('../controllers/ambientes.controllers');
 
 
 
@@ -9,7 +9,6 @@ router.post('/ambientes', upload.single('image'), postAmbientes);
 router.get('/ambientes', getAmbientes);
 router.get('/ambientes/:nome', getAmbienteByName);
 router.delete('/ambientes/:id', deleteAmbientes);
-router.get('/ambientes/:id', getAmbienteById);
 router.put('/ambientes/:id', upload.single('image'), updateAmbiente);
 
 
