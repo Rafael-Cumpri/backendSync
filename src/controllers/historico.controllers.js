74031cdc766e5feb8f9bc5dbfcef9cb671430061
Marinamorais <marinamorais@aluno.senai.br>
2024-11-13 09:45:35 -0300

    feat: adicionar função para devolver todos os ambientes e ajustar formatação de código

--- src/controllers/historico.controllers.js
+++ src/controllers/historico.controllers.js
@@ -19,7 +19,7 @@ async function newPromiseClass(req, res) {
         await pool.query(query, values);
         await pool.query('UPDATE ambientes SET disponivel = false WHERE numero_ambiente = $1', [ambiente]);
         const responseAmbiente = await pool.query('SELECT * FROM ambientes WHERE numero_ambiente = $1', [ambiente]);
-        if(responseAmbiente.rows[0].chave === true) {
+        if (responseAmbiente.rows[0].chave === true) {
             await pool.query('UPDATE chaves SET disponivel = false WHERE salas = $1', [ambiente]);
         }
         res.status(200).json({ message: 'Sala reservada com sucesso' });
@@ -94,7 +94,7 @@ async function getHistoricoInfos(req, res) {
     }
 }
 
-async function deleteHistorico(req, res){
+async function deleteHistorico(req, res) {
     try {
         const { id } = req.params;
         await pool.query('DELETE FROM historico WHERE id = $1', [id]);
@@ -109,9 +109,9 @@ async function updateHistorico(req, res) {
     try {
         const { id } = req.params;
         const { data_inicio, funcionario, ambiente } = req.body;
-            await pool.query('UPDATE historico SET data_inicio = $1, funcionario = $2, ambiente = $3  WHERE id = $4', [ data_inicio, funcionario, ambiente, id]);
-            res.status(200).send({ mensagem: 'historico atualizado' });
-        
+        await pool.query('UPDATE historico SET data_inicio = $1, funcionario = $2, ambiente = $3  WHERE id = $4', [data_inicio, funcionario, ambiente, id]);
+        res.status(200).send({ mensagem: 'historico atualizado' });
+
     } catch (error) {
         console.error('erro ao atualizar historico', error);
         res.status(500).send('erro ao atualizar historico');
@@ -127,13 +127,37 @@ async function devolverAmbiente(req, res) {
 
         const responseAmbiente = await pool.query('SELECT * FROM ambientes WHERE numero_ambiente = $1', [ambiente]);
         console.log("Ambiente após atualização:", responseAmbiente.rows[0]);
-        if(responseAmbiente.rows[0].chave === true) {
+        if (responseAmbiente.rows[0].chave === true) {
             await pool.query('UPDATE chaves SET disponivel = true WHERE salas = $1', [ambiente]);
         }
-        res.status(200).send({ mensagem: 'atualizado com sucesso'})
+        res.status(200).send({ mensagem: 'atualizado com sucesso' })
     } catch (err) {
         console.error('erro ao devolver ambiente', err)
         res.status(500).send('Erro ao devolver ambiente')
+    }
+}
+async function devolverTodosAmbientes(req, res) {
+    try {
+        const { data_fim, usuario } = req.body;
+        console.log(`Devolvendo todos os ambientes do usuario com NIF: ${usuario}`);
+
+        await pool.query('UPDATE historico SET data_fim = $1 WHERE funcionario = $2 AND data_fim IS NULL', [data_fim, usuario]);
+
+        const responseHistorico = await pool.query('SELECT ambiente FROM historico WHERE funcionario = $1 AND data_fim = $2', [usuario, data_fim]);
+
+        for (const row of responseHistorico.rows) {
+            const ambiente = row.ambiente;
+            await pool.query('UPDATE ambientes SET disponivel = true WHERE numero_ambiente = $1', [ambiente]);
+            const responseAmbiente = await pool.query('SELECT * FROM ambientes WHERE numero_ambiente = $1', [ambiente]);
+            if (responseAmbiente.rows[0].chave === true) {
+                await pool.query('UPDATE chaves SET disponivel = true WHERE salas = $1', [ambiente]);
+            }
+        }
+
+        res.status(200).send({ mensagem: 'atualizado com sucesso' });
+    } catch (err) {
+        console.error('erro ao devolver ambiente', err);
+        res.status(500).send('Erro ao devolver ambiente');
     }
 }
 
@@ -141,17 +165,17 @@ async function devolverAmbienteADM(req, res) {
     try {
         const { id } = req.params;
         const { data_fim } = req.body;
-        
+
         await pool.query('UPDATE historico SET data_fim = $1 WHERE id = $2', [data_fim, id]);
         const responseHistorico = await pool.query('SELECT * FROM historico WHERE id = $1', [id]);
         await pool.query('UPDATE ambientes SET disponivel = true WHERE numero_ambiente = $1', [responseHistorico.rows[0].ambiente]);
-        
+
         const responseAmbiente = await pool.query('SELECT * FROM ambientes WHERE numero_ambiente = $1', [id]);
-        
+
         if (responseAmbiente.rows[0]?.chave === 'true') {
             await pool.query('UPDATE chaves SET disponivel = true WHERE salas = $1', [id]);
         }
-        
+
         res.status(200).send({ mensagem: 'atualizado com sucesso' });
     } catch (err) {
         console.error('erro ao devolver ambiente', err);
@@ -185,4 +209,4 @@ async function getFullHistorico(req, res) {
 }
 
 
-module.exports = { newPromiseClass, getHistorico, deleteHistorico, updateHistorico, devolverAmbiente, getHistoricoInfos, devolverAmbienteADM, getFullHistorico };
+module.exports = { newPromiseClass, getHistorico, deleteHistorico, updateHistorico, devolverAmbiente, getHistoricoInfos, devolverAmbienteADM, getFullHistorico, devolverTodosAmbientes };
