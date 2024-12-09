fe829809cab68185fc0eb58f705025f1b962ba02
Maria Rita Gomes <mariagomes@aluno.senai.br>
2024-12-09 09:47:48 -0300

    feat: esqueci de subir

--- src/routes/salasFixas.routes.js
+++ src/routes/salasFixas.routes.js
@@ -10,6 +10,6 @@ const {
 // Rotas para salas fixas
 router.post('/salas-fixas', addFixedClass);               // Adicionar uma sala fixa
 router.get('/salas_fixas/:usuario_id', getFixedClasses);      // Obter salas fixas de um usuário
-router.delete('/salas-fixas/:id', deleteFixedClass);           // Deletar uma sala fixa
+router.delete('/salas_fixas/:id', deleteFixedClass);           // Deletar uma sala fixa
 router.put('/salas-fixas/:id', updateFixedClass);              // Atualizar uma sala fixa
 module.exports = router;
